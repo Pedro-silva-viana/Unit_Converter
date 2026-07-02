225ad9cb64eb50578d264151f35e180f1574dfae
Pedro Viana <pedrovianaiguatu@gmail.com>
2026-07-02 09:54:27 -0300

    feat: implement core conversion logic classes
    -Add WeightComverter logic.
    
    -Add LengthConverter logic.

--- src/main/java/com/example/LengthConverter.java
+++ src/main/java/com/example/LengthConverter.java
@@ -1,5 +1,67 @@
 package com.example;
 
 public class LengthConverter {
+
+    public double convertM(double amount, String mode){
+        switch (mode) {
+            case "mm":
+                amount = amount / (1000);
+                break;
+            case "cm":
+                amount = amount / (1000);
+                break;
+            case "m":
+                break;
+            case "km":
+                amount = amount * (1000);
+                break;
+            case "in":
+                amount = amount / (39.35);
+                break;
+            case "ft":
+                amount = amount / (3.281);
+                break;
+            case "yd":
+                amount = amount / (1.094);
+                break;
+            case "mi":
+                amount = amount * (1609);
+                break;
+            default:
+                break;
+        }
+        return amount;
+    }
+
+    public double MToOthers(double amount, String mode){
+        switch (mode) {
+            case "Mm":
+                amount = amount / (1000);
+                break;
+            case "Cm":
+                amount = amount / (1000);
+                break;
+            case "Me":
+                break;
+            case "Km":
+                amount = amount * (1000);
+                break;
+            case "In":
+                amount = amount / (39.35);
+                break;
+            case "Ft":
+                amount = amount / (3.281);
+                break;
+            case "Yd":
+                amount = amount / (1.094);
+                break;
+            case "Mi":
+                amount = amount * (1609);
+                break;
+            default:
+                break;
+        }
+        return amount;
+    }
     
 }
--- src/main/java/com/example/WeightConverter.java
+++ src/main/java/com/example/WeightConverter.java
@@ -1,5 +1,49 @@
 package com.example;
 
 public class WeightConverter {
-    
+
+    public double convertKil(double amount, String mode){
+        switch (mode) {
+            case "mg":
+                amount = amount / (1000000);
+                break;
+            case "g":
+                amount = amount / (1000);
+                break;
+            case "kg":
+                break;
+            case "oz":
+                amount = amount / (35.274);
+                break;
+            case "lbs":
+                amount = amount / (2.205);
+                break;
+            default:
+                break;
+        }
+        return amount;
+    }
+
+    public double KilToOthers(double amount, String mode){
+        switch (mode) {
+            case "mg":
+                amount = amount * (1000000);
+                break;
+            case "g":
+                amount = amount * (1000);
+                break;
+            case "kg":
+                break;
+            case "oz":
+                amount = amount * (35.274);
+                break;
+            case "lbs":
+                amount = amount * (2.205);
+                break;
+            default:
+                break;
+        }
+        return amount;
+    }
+
 }
